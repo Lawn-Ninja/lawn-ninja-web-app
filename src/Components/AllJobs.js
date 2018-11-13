ed1ccfc1fdd0144b1293ac81573b99de5054b755
coryrgriffith <corygriffith9@gmail.com>
2018-11-13 10:58:24 -0600

    fix AllJobs component to render all the jobs in the rails api

--- src/Components/AllJobs.js
+++ src/Components/AllJobs.js
@@ -1,34 +1,39 @@
-import React, {Component} from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 class AllJobs extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       jobs: []
-    }
+    };
   }
+
   componentDidMount() {
-    axios.get('http://localhost:3001/jobs').then(response => {console.log(response)
-      this.setState({jobs: response.data})
-    })
-    .catch(error => console.log(error))
+    axios
+      .get("http://localhost:3001/jobs")
+      .then(response => {
+        console.log(response);
+        this.setState({ jobs: response.data.jobs });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
       <div>
-        {this.state.jobs.map((job) => {
-          return(
+        {this.state.jobs.map(job => {
+          return (
             <div className="tile" key={job.id}>
-              <h4>{job.status}</h4>
-              <h4>{job.requested_time}</h4>
+              <h4>User ID: {job.user_id}</h4>
+              <p>Requested Time: {job.requested_time}</p>
+              <p>Status: {job.status}</p>
             </div>
-          )
+          );
         })}
       </div>
     );
   }
 }
 
-export default AllJobs
+export default AllJobs;
