64720efcddf9f888151a5224d812b1e5e10d65a8
Ines Daniel <i.danie8@gmail.com>
2018-11-22 14:00:24 -0600

    make home page render not logged in page, link navbar to routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./Containers/Home";
+
+// import LandingPage from "./Components/LandingPage";
 // import AllJobs from "./Components/AllJobs";
-import NewUser from "./Components/NewUser";
-import LandingPage from "./Components/LandingPage";
-import Login from "./Components/Login";
+// import NewUser from "./Components/NewUser";
+// import Login from "./Components/Login";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <div className="header">
-            <nav className='navbar'>
-              <div className='logo'>
-                Lawn<span>Ninja</span>
-              </div>
-              <ul>
-                <li>
-                  <a href="/" className="hover_link">HOME</a>
-                </li>
-                <li>
-                  <a href="/" className="hover_link">ABOUT US</a>
-                </li>
-                <li>
-                  <a href="/" className="hover_link">CONTACT US</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-
-          {/* BEGINNING OF LANDING PAGE SAMPLES */}
-          Logged Out:
-          <LandingPage />
-          {/* END OF LANDING PAGE SAMPLES */}
-
-          <Login />
-          <NewUser />
+          <Home />
+          {/* <LandingPage /> */}
+          {/* <Login /> */}
+          {/* <NewUser /> */}
           {/* <AllJobs /> */}
         </div>
       </BrowserRouter>
---
+++ src/Containers/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+// import axios from "axios";
+import { Route, Link } from "react-router-dom";
+
+import "./Home.css";
+import LandingPage from "../Components/LandingPage";
+
+class Home extends Component {
+    render () {
+        return (
+            <div className="Home">
+                <div className="header">
+                    <nav className='navbar'>
+                        <div className='logo'>
+                        Lawn<span>Ninja</span>
+                        </div>
+                        <ul>
+                        <li>
+                            <Link to="/" className="hover_link">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login" className="hover_link">LogIn</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup" className="hover_link">SignUp</Link>
+                        </li>
+                        </ul>
+                    </nav>
+                </div>
+                <Route path="/" exact component={LandingPage} />
+            </div>
+        );
+    }
+}
+
+export default Home;
