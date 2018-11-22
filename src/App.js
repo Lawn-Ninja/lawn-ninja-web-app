e6131dd3638e6ccdddc0bc8090c76e41921c37b4
Ines Daniel <i.danie8@gmail.com>
2018-11-22 12:03:26 -0600

    install react-router, add BrowserRouter/links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom"
 import "./App.css";
 // import AllJobs from "./Components/AllJobs";
 import NewUser from "./Components/NewUser";
@@ -8,39 +9,37 @@ import Login from "./Components/Login";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="header">
-          <nav className='navbar'>
-            <div className='logo'>
-              Lawn<span>Ninja</span>
-            </div>
-            <ul>
-              <li>
-                <a href="/" className="hover_link">HOME</a>
-              </li>
-              <li>
-                <a href="/" className="hover_link">ABOUT US</a>
-              </li>
-              <li>
-                <a href="/" className="hover_link">CONTACT US</a>
-              </li>
-            </ul>
-          </nav>
+      <BrowserRouter>
+        <div className="App">
+          <div className="header">
+            <nav className='navbar'>
+              <div className='logo'>
+                Lawn<span>Ninja</span>
+              </div>
+              <ul>
+                <li>
+                  <a href="/" className="hover_link">HOME</a>
+                </li>
+                <li>
+                  <a href="/" className="hover_link">ABOUT US</a>
+                </li>
+                <li>
+                  <a href="/" className="hover_link">CONTACT US</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          {/* BEGINNING OF LANDING PAGE SAMPLES */}
+          Logged Out:
+          <LandingPage />
+          {/* END OF LANDING PAGE SAMPLES */}
+
+          <Login />
+          <NewUser />
+          {/* <AllJobs /> */}
         </div>
-
-        {/* BEGINNING OF LANDING PAGE SAMPLES */}
-        Logged In, Not a Provider:
-        <LandingPage loggedIn={true} provider={false} />
-        Logged In, Provider:
-        <LandingPage loggedIn={true} provider={true} />
-        Logged Out:
-        <LandingPage loggedIn={false} />
-        {/* END OF LANDING PAGE SAMPLES */}
-
-        <Login />
-        <NewUser />
-        {/* <AllJobs /> */}
-      </div>
+      </BrowserRouter>
     );
   }
 }
