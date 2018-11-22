b3d299a356acbc257d9adcdbcfc0196cf9004076
Ines Daniel <i.danie8@gmail.com>
2018-11-22 11:20:13 -0600

    install jquery

--- src/Components/AllJobs.js
+++ src/Components/AllJobs.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
 import Job from "./Job";
+import $ from 'jquery';
 
 class AllJobs extends Component {
   constructor(props) {
@@ -11,18 +11,23 @@ class AllJobs extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get("http://localhost:3001/jobs")
-      .then(response => {
-        console.log(response);
-        this.setState({ jobs: response.data.jobs });
-      })
-      .catch(error => console.log(error));
+    let token = "Bearer " + localStorage.getItem('id_token');
+
+    $.ajax({
+      url: "http://localhost:3001/jobs",
+      type: "GET",
+      beforeSend: function(xhr){xhr.setRequestHeader('Authorization', token)},
+      context: this, // Allows us to use this.setState inside success
+      success: (result) => {
+        this.setState({jobs: result.jobs});
+      }
+    })
   }
 
   render() {
     return (
       <div>
+        {console.log(this.state)}
         {this.state.jobs.map(job => {
           return (
             <div className="tile" key={job.id}>
