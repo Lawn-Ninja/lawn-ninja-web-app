9b620a6167359d7bae65868c6ffc8798edf7eea8
coryrgriffith <corygriffith9@gmail.com>
2018-11-24 16:51:20 -0600

    WIP NewJob component to create a new job

---
+++ src/Components/NewJob.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import TextInputGroup from "./TextInputGroup";
+import axios from "axios";
+
+class NewJob extends Component {
+  state = {
+    requested_time: "",
+    errors: {}
+  };
+  render() {
+    return <div />;
+  }
+}
+export default NewJob;
