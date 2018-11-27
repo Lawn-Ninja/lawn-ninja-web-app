e52723311036bb18edd981fea162d7154a0a42d3
Heather Lee <heather.k.lee@gmail.com>
2018-11-26 19:39:43 -0500

    import MyJobs.css in JobsNearMe.js

--- src/Components/Job.js
+++ src/Components/Job.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "./MyJobs.css";
 
 class Job extends Component {
   render() {
     const { user_id, requested_time, status } = this.props.job;
 
     return (
-      <div>
+      <div className="my-jobs">
         <h4>User ID: {user_id}</h4>
         <p>Requested Time: {requested_time}</p>
         <p>Status: {status}</p>
